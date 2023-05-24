bae62cd3cf1924e1c18b7f0a31653b9bbed6d3b1
TanPham061293 <134413763+TanPham061293@users.noreply.github.com>
2023-05-24 11:39:31 +0700

    C#

---
+++ Game_Show_Trac_Nghiem/Form1.Designer.cs
@@ -0,0 +1,210 @@
+﻿namespace Game_Show_Trac_Nghiem
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            lb_Chaomung = new Label();
+            lb_tracnghiem = new Label();
+            bt_newgame = new Button();
+            gb_Danhsach_cauhoi = new GroupBox();
+            gb_Dapan = new GroupBox();
+            gb_luachongoi_cauhoi = new GroupBox();
+            rb_cauhoi_hacknao = new RadioButton();
+            rb_cauhoi_vui = new RadioButton();
+            ptb_anhDauGame = new PictureBox();
+            lb_Cauhoi = new Label();
+            bt_Next = new Button();
+            bt_Thoatgame = new Button();
+            gb_luachongoi_cauhoi.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)ptb_anhDauGame).BeginInit();
+            SuspendLayout();
+            // 
+            // lb_Chaomung
+            // 
+            lb_Chaomung.AutoSize = true;
+            lb_Chaomung.Font = new Font("Times New Roman", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            lb_Chaomung.Location = new Point(552, 58);
+            lb_Chaomung.Name = "lb_Chaomung";
+            lb_Chaomung.Size = new Size(236, 19);
+            lb_Chaomung.TabIndex = 0;
+            lb_Chaomung.Text = "Chào Mừng Bạn Đến Game Show";
+           
+            // 
+            // lb_tracnghiem
+            // 
+            lb_tracnghiem.AutoSize = true;
+            lb_tracnghiem.Location = new Point(272, 23);
+            lb_tracnghiem.Name = "lb_tracnghiem";
+            lb_tracnghiem.Size = new Size(168, 15);
+            lb_tracnghiem.TabIndex = 1;
+            lb_tracnghiem.Text = "Trắc Nghiệm Vui....ta.daaaaaaa";
+            // 
+            // bt_newgame
+            // 
+            bt_newgame.BackColor = Color.FromArgb(128, 255, 128);
+            bt_newgame.Location = new Point(464, 15);
+            bt_newgame.Name = "bt_newgame";
+            bt_newgame.Size = new Size(95, 23);
+            bt_newgame.TabIndex = 2;
+            bt_newgame.Text = "New Game.";
+            bt_newgame.UseVisualStyleBackColor = false;
+            bt_newgame.Click += bt_newgame_Click;
+            // 
+            // gb_Danhsach_cauhoi
+            // 
+            gb_Danhsach_cauhoi.Location = new Point(12, 311);
+            gb_Danhsach_cauhoi.Name = "gb_Danhsach_cauhoi";
+            gb_Danhsach_cauhoi.Size = new Size(730, 100);
+            gb_Danhsach_cauhoi.TabIndex = 3;
+            gb_Danhsach_cauhoi.TabStop = false;
+            gb_Danhsach_cauhoi.Text = "Danh sách câu hỏi:";
+            // 
+            // gb_Dapan
+            // 
+            gb_Dapan.Location = new Point(12, 139);
+            gb_Dapan.Name = "gb_Dapan";
+            gb_Dapan.Size = new Size(730, 152);
+            gb_Dapan.TabIndex = 6;
+            gb_Dapan.TabStop = false;
+            // 
+            // gb_luachongoi_cauhoi
+            // 
+            gb_luachongoi_cauhoi.Controls.Add(rb_cauhoi_hacknao);
+            gb_luachongoi_cauhoi.Controls.Add(rb_cauhoi_vui);
+            gb_luachongoi_cauhoi.Location = new Point(117, 44);
+            gb_luachongoi_cauhoi.Name = "gb_luachongoi_cauhoi";
+            gb_luachongoi_cauhoi.Size = new Size(225, 53);
+            gb_luachongoi_cauhoi.TabIndex = 4;
+            gb_luachongoi_cauhoi.TabStop = false;
+            gb_luachongoi_cauhoi.Text = "Lựa chọn gói câu hỏi:";
+            // 
+            // rb_cauhoi_hacknao
+            // 
+            rb_cauhoi_hacknao.AutoSize = true;
+            rb_cauhoi_hacknao.Location = new Point(175, 28);
+            rb_cauhoi_hacknao.Name = "rb_cauhoi_hacknao";
+            rb_cauhoi_hacknao.Size = new Size(121, 19);
+            rb_cauhoi_hacknao.TabIndex = 5;
+            rb_cauhoi_hacknao.TabStop = true;
+            rb_cauhoi_hacknao.Text = "Câu hỏi HackNao.";
+            rb_cauhoi_hacknao.UseVisualStyleBackColor = true;
+            rb_cauhoi_hacknao.CheckedChanged += rb_cauhoi_hacknao_CheckedChanged;
+            // 
+            // rb_cauhoi_vui
+            // 
+            rb_cauhoi_vui.AutoSize = true;
+            rb_cauhoi_vui.Location = new Point(19, 28);
+            rb_cauhoi_vui.Name = "rb_cauhoi_vui";
+            rb_cauhoi_vui.Size = new Size(89, 19);
+            rb_cauhoi_vui.TabIndex = 6;
+            rb_cauhoi_vui.TabStop = true;
+            rb_cauhoi_vui.Text = "Câu hỏi Vui.";
+            rb_cauhoi_vui.UseVisualStyleBackColor = true;
+            rb_cauhoi_vui.CheckedChanged += rb_cauhoi_vui_CheckedChanged;
+            // 
+            // ptb_anhDauGame
+            // 
+            ptb_anhDauGame.BackColor = Color.FromArgb(255, 255, 192);
+            ptb_anhDauGame.Location = new Point(12, 41);
+            ptb_anhDauGame.Name = "ptb_anhDauGame";
+            ptb_anhDauGame.Size = new Size(87, 50);
+            ptb_anhDauGame.TabIndex = 5;
+            ptb_anhDauGame.TabStop = false;
+            // 
+            // lb_Cauhoi
+            // 
+            lb_Cauhoi.Location = new Point(12, 100);
+            lb_Cauhoi.Name = "lb_Cauhoi";
+            lb_Cauhoi.Size = new Size(730, 53);
+            lb_Cauhoi.TabIndex = 7;
+            lb_Cauhoi.Text = "Câu hỏi";
+            // 
+            // bt_Next
+            // 
+            bt_Next.AllowDrop = true;
+            bt_Next.BackColor = Color.FromArgb(128, 255, 128);
+            bt_Next.Location = new Point(386, 58);
+            bt_Next.Name = "bt_Next";
+            bt_Next.Size = new Size(75, 23);
+            bt_Next.TabIndex = 8;
+            bt_Next.Text = "Câu tiếp.";
+            bt_Next.UseVisualStyleBackColor = false;
+            bt_Next.Click += bt_Next_Click;
+            // 
+            // bt_Thoatgame
+            // 
+            bt_Thoatgame.BackColor = Color.FromArgb(255, 128, 0);
+            bt_Thoatgame.Location = new Point(70, 10);
+            bt_Thoatgame.Name = "bt_Thoatgame";
+            bt_Thoatgame.Size = new Size(94, 28);
+            bt_Thoatgame.TabIndex = 9;
+            bt_Thoatgame.Text = "Thoát Game.";
+            bt_Thoatgame.UseVisualStyleBackColor = false;
+            bt_Thoatgame.Click += bt_Thoatgame_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(bt_Thoatgame);
+            Controls.Add(bt_Next);
+            Controls.Add(lb_Cauhoi);
+            Controls.Add(gb_Dapan);
+            Controls.Add(ptb_anhDauGame);
+            Controls.Add(gb_luachongoi_cauhoi);
+            Controls.Add(gb_Danhsach_cauhoi);
+            Controls.Add(bt_newgame);
+            Controls.Add(lb_tracnghiem);
+            Controls.Add(lb_Chaomung);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            gb_luachongoi_cauhoi.ResumeLayout(false);
+            gb_luachongoi_cauhoi.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)ptb_anhDauGame).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label lb_Chaomung;
+        private Label lb_tracnghiem;
+        private Button bt_newgame;
+        private GroupBox gb_Danhsach_cauhoi;
+        private GroupBox gb_luachongoi_cauhoi;
+        private RadioButton rb_cauhoi_hacknao;
+        private RadioButton rb_cauhoi_vui;
+        private PictureBox ptb_anhDauGame;
+        private GroupBox gb_Dapan;
+        private Label lb_Cauhoi;
+        private Button bt_Next;
+        private Button bt_Thoatgame;
+    }
+}
---
+++ Game_Show_Trac_Nghiem/Form1.cs
@@ -0,0 +1,374 @@
+﻿using System.Diagnostics.Eventing.Reader;
+
+namespace Game_Show_Trac_Nghiem
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string Dia_chi_Cau_Cau_Hoi_Vui = "Danh_Sach_Cau_Hoi_Vui.txt";
+        string Dia_chi_Cau_Cau_Hoi_HackNao = "Danh_Sach_Cau_Hoi_HackNao.txt";
+        List<string> Danh_Sach_Cau_Hoi = new List<string>();
+        string Dia_Chi_Dap_An_Vui = "Dap_An_Cau_Hoi_Vui.txt";
+        string Dia_Chi_Dap_An_HackNao = "Dap_An_Cau_Hoi_HackNao.txt";
+        List<string> arr_Dapan_Vui = new List<string>();
+        List<RadioButton> arr_gb_Cauhoi = new List<RadioButton>();
+        List<string> abc = new List<string>(); // danh sách lựa con abc theo từng câu
+        List<List<string>> Danh_Sach_Dap_An_Theo_Cau = new List<List<string>>(); // danh sách chưa abc theo câu
+        List<RadioButton> arr_rb = new List<RadioButton>(); // danh sách chưa mảng radiobutton
+        int Index = 0; // chỉ số theo câu hỏi tương ứng 
+        int ChiSo_xetDKLocation = 0; // để xét dk location các radiobutton
+        int ChiSo_xetDKLocation2 = 0;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ptb_anhDauGame.Visible = false; // ẩn các control chưa dùng đến
+            gb_luachongoi_cauhoi.Visible = false;
+            lb_Cauhoi.Visible = false;
+            gb_Dapan.Visible = false;
+            gb_Danhsach_cauhoi.Visible = false;
+
+            bt_Next.Visible = false;
+            bt_Thoatgame.Visible = false;
+            //this.BackgroundImage = Image.FromFile("C:\\Users\\PC\\Desktop\\Anh_BackGroud\\1.jpg");
+            //this.BackgroundImageLayout = ImageLayout.Stretch;
+            lb_Chaomung.Location = new Point(255, 115);
+            lb_Chaomung.Font = new Font("Times New Roman", 15F, FontStyle.Bold, GraphicsUnit.Point);
+
+            lb_tracnghiem.Location = new Point(lb_Chaomung.Location.X + (lb_Chaomung.Size.Width) / 2 - (lb_tracnghiem.Size.Width) / 2, 180);
+            lb_tracnghiem.Font = new Font("Times New Roman", 15F, FontStyle.Italic, GraphicsUnit.Point);
+
+            bt_newgame.Location = new Point(lb_tracnghiem.Location.X + (lb_tracnghiem.Size.Width) / 2 - (bt_newgame.Size.Width) / 2, 246);
+            bt_newgame.Font = new Font("VNI-Ariston", 12F, FontStyle.Underline, GraphicsUnit.Point);
+            bt_newgame.AutoSize = true;
+            if (!File.Exists(Dia_chi_Cau_Cau_Hoi_Vui) || !File.Exists(Dia_Chi_Dap_An_Vui)) // kiểm tra địa chỉ file
+            {
+                MessageBox.Show("Địa chỉ File không chính xác.");
+            }
+        }
+
+        private void bt_newgame_Click(object sender, EventArgs e)
+        {
+            lb_Chaomung.Visible = false;
+            lb_tracnghiem.Visible = false;
+            bt_newgame.Visible = false;
+
+            ptb_anhDauGame.Visible = true;
+            ptb_anhDauGame.Location = new Point(185, 44);
+            ptb_anhDauGame.Size = new Size(359, 245);
+            ptb_anhDauGame.BackgroundImage = Image.FromFile("Anh_BackGroud\\3.jpg");
+            ptb_anhDauGame.BackgroundImageLayout = ImageLayout.Stretch;
+
+            gb_luachongoi_cauhoi.Visible = true;
+            gb_luachongoi_cauhoi.Location = new Point(185, 317);
+            gb_luachongoi_cauhoi.Size = new Size(359, 53);
+        }
+        void Hanhdongkhicheck_true()
+        {
+            if (rb_cauhoi_vui.Checked == true)
+            {
+                DialogResult luachon = MessageBox.Show("Bạn đã chắc lựa chọn gói câu hỏi vui.", "Thông báo.", MessageBoxButtons.YesNo, MessageBoxIcon.Information);
+                if (luachon == DialogResult.Yes)
+                {
+                    ptb_anhDauGame.Visible = false;
+                    gb_luachongoi_cauhoi.Visible = false;
+
+                    lb_Cauhoi.Visible = true;
+                    lb_Cauhoi.Location = new Point(154, 100);
+                    lb_Cauhoi.Text = Danh_Sach_Cau_Hoi[Index].ToString();
+                    TaoRadioButton();
+
+                    gb_Dapan.Visible = true;
+                    gb_Dapan.Location = new Point(154, 100 + lb_Cauhoi.Size.Height + 20);
+                    gb_Dapan.BackgroundImage = Image.FromFile("Anh_BackGroud\\2.jpg");
+
+                    bt_Thoatgame.Visible = true;
+                    bt_Next.Location = new Point(lb_Cauhoi.Location.X + lb_Cauhoi.Size.Width - 50, 10);
+                    bt_Next.Font = new Font("VNI-Ariston", 12F, FontStyle.Underline, GraphicsUnit.Point);
+                    bt_Next.AutoSize = true;
+                    bt_Next.Visible = true;
+
+
+                    for (int i = 0; i < Danh_Sach_Cau_Hoi.Count; i++)
+                    {
+                        RadioButton rb1 = new RadioButton();
+                        rb1.Text = "Câu: " + (i + 1).ToString();
+                        rb1.AutoSize = true;
+                        rb1.BackColor = Color.Beige;
+                        rb1.Enabled = false;
+
+                        if (i == 0)
+                        {
+                            rb1.Location = new Point(5, 20);
+
+                        }
+                        else if (i == 1)
+                        {
+                            rb1.Location = new Point(5, 40);
+                        }
+
+                        else
+                        {
+                            rb1.Location = new Point(arr_gb_Cauhoi[arr_gb_Cauhoi.Count - 2].Location.X + 10 + (arr_gb_Cauhoi[arr_gb_Cauhoi.Count - 2].Size.Width), arr_gb_Cauhoi[arr_gb_Cauhoi.Count - 2].Location.Y);
+
+                        }
+                        ChiSo_xetDKLocation2++;
+                        arr_gb_Cauhoi.Add(rb1);
+                        this.gb_Danhsach_cauhoi.Controls.Add(rb1);
+                    }
+                    arr_gb_Cauhoi[Index].BackColor = Color.LightPink;
+                    gb_Danhsach_cauhoi.Visible = true;
+                    gb_Danhsach_cauhoi.AutoSize = true;
+                    gb_Danhsach_cauhoi.Location = new Point(gb_Dapan.Location.X, gb_Dapan.Location.Y + gb_Dapan.Size.Height + 20);
+                }
+            }
+            if (rb_cauhoi_hacknao.Checked == true)
+            {
+                DialogResult luachon = MessageBox.Show("Bạn đã chắc lựa chọn gói câu hỏi HackNao.", "Thông báo.", MessageBoxButtons.YesNo, MessageBoxIcon.Information);
+                if (luachon == DialogResult.Yes)
+                {
+                    ptb_anhDauGame.Visible = false;
+                    gb_luachongoi_cauhoi.Visible = false;
+
+                    lb_Cauhoi.Visible = true;
+                    lb_Cauhoi.Location = new Point(154, 100);
+                    lb_Cauhoi.Text = Danh_Sach_Cau_Hoi[Index].ToString();
+                    TaoRadioButton();
+
+                    gb_Dapan.Visible = true;
+                    gb_Dapan.Location = new Point(154, 100 + lb_Cauhoi.Size.Height + 20);
+                    gb_Dapan.BackgroundImage = Image.FromFile("Anh_BackGroud\\1.jpg");
+
+                    bt_Thoatgame.Visible = true;
+                    bt_Next.Location = new Point(lb_Cauhoi.Location.X + lb_Cauhoi.Size.Width - 50, 10);
+                    bt_Next.Font = new Font("Times New Roman", 12F, FontStyle.Underline, GraphicsUnit.Point);
+                    bt_Next.AutoSize = true;
+                    bt_Next.Visible = true;
+
+
+                    for (int i = 0; i < Danh_Sach_Cau_Hoi.Count; i++)
+                    {
+                        RadioButton rb1 = new RadioButton();
+                        rb1.Text = "Câu: " + (i + 1).ToString();
+                        rb1.AutoSize = true;
+                        rb1.BackColor = Color.Beige;
+                        rb1.Enabled = false;
+
+                        if (i == 0)
+                        {
+                            rb1.Location = new Point(5, 20);
+
+                        }
+                        else if (i == 1)
+                        {
+                            rb1.Location = new Point(5, 40);
+                        }
+
+                        else
+                        {
+                            rb1.Location = new Point(arr_gb_Cauhoi[arr_gb_Cauhoi.Count - 2].Location.X + 10 + (arr_gb_Cauhoi[arr_gb_Cauhoi.Count - 2].Size.Width), arr_gb_Cauhoi[arr_gb_Cauhoi.Count - 2].Location.Y);
+                        }
+                        arr_gb_Cauhoi.Add(rb1);
+                        this.gb_Danhsach_cauhoi.Controls.Add(rb1);
+                    }
+                    arr_gb_Cauhoi[Index].BackColor = Color.LightPink;
+                    gb_Danhsach_cauhoi.Visible = true;
+                    gb_Danhsach_cauhoi.AutoSize = true;
+                    gb_Danhsach_cauhoi.Location = new Point(gb_Dapan.Location.X, gb_Dapan.Location.Y + gb_Dapan.Size.Height + 20);
+                }
+            }
+        }
+        private void rb_cauhoi_vui_CheckedChanged(object sender, EventArgs e)
+        {
+            bt_Next.Enabled = false;
+            Danh_Sach_Cau_Hoi.Clear();
+            Danh_Sach_Dap_An_Theo_Cau.Clear();
+            arr_rb.Clear();
+            gb_Dapan.Controls.Clear();
+            ChiSo_xetDKLocation = 0;
+            Index = 0;
+            if (rb_cauhoi_vui.Checked == true)
+            {
+                foreach (var item in File.ReadLines(Dia_chi_Cau_Cau_Hoi_Vui)) // đọc câu hỏi từ file
+                {
+                    if (item.Contains("A.") || item.Contains("B.") || item.Contains("C.") || item.Contains("D."))
+                    {
+                        abc.Add(item); // đưa ựa chọn vào abc
+                    }
+                    else if (item != "")
+                    {
+                        Danh_Sach_Cau_Hoi.Add(item); // đưa câu hỏi vào danh sách
+                        if (abc.Count != 0)
+                        {
+                            // cần tạo thêm 1 mảng gán bằng abc và thêm vào danh sách đáp án theo câu. nếu không kho clear abc thì danh sách đáp án theo câu sẻ bị rỗng
+                            Danh_Sach_Dap_An_Theo_Cau.Add(new List<String>(abc));// không dduocj thêm trực tiếp abc
+                            abc.Clear();
+                        }
+                    }
+                }
+                if (Danh_Sach_Cau_Hoi.Count != Danh_Sach_Dap_An_Theo_Cau.Count)// đưa các lựa chọn câu hỏi cuối cùng vào
+                {
+                    Danh_Sach_Dap_An_Theo_Cau.Add(new List<String>(abc));
+                    abc.Clear();
+                }
+
+                foreach (var item3 in File.ReadAllLines(Dia_Chi_Dap_An_Vui)) // đưa đáp án vào mảng từ file
+                {
+                    arr_Dapan_Vui.Add(item3);
+                }
+                Hanhdongkhicheck_true();
+            }
+        }
+        void TaoRadioButton()
+        {
+            foreach (var item in Danh_Sach_Dap_An_Theo_Cau[Index])
+            {
+                RadioButton rb = new RadioButton();
+                rb.Text = item;
+                rb.AutoSize = true;
+                rb.BackColor = Color.DarkKhaki;
+                if (ChiSo_xetDKLocation != 0)
+                {
+                    rb.Location = new Point(arr_rb[arr_rb.Count - 1].Location.X, arr_rb[arr_rb.Count - 1].Location.Y + 10 + (arr_rb[arr_rb.Count - 1].Size.Height));
+                }
+                ChiSo_xetDKLocation++;
+                rb.Click += Rb_Click;
+                arr_rb.Add(rb);
+                this.gb_Dapan.Controls.Add(rb);
+            }
+        }
+
+        private void Rb_Click(object? sender, EventArgs e)
+        {
+            TrangthaiCheck();
+        }
+
+        void TrangthaiCheck()
+        {
+            foreach (var item in arr_rb)
+            {
+                if (item.Checked == true)
+                {
+                    DialogResult luachon = MessageBox.Show("Đáp án của bạn la:" + item.Text, "Thông báo.", MessageBoxButtons.YesNo);
+                    if (luachon == DialogResult.Yes)
+                    {
+                        int ChiSo_dauCach = item.Text.IndexOf(" ");
+                        string str = item.Text.Substring(ChiSo_dauCach + 1);
+                        bool check = false;
+                        foreach (var item1 in arr_Dapan_Vui)
+                        {
+                            if (str.CompareTo(item1) == 0)
+                            {
+                                if (Index != Danh_Sach_Cau_Hoi.Count - 1)
+                                {
+                                    DialogResult luachon2 = MessageBox.Show("Bạn đã trả lời chính xác.");
+
+                                    bt_Next.Enabled = true;
+                                }
+                                else
+                                {
+                                    MessageBox.Show("Bạn đã trả lời chính xác.");
+                                    MessageBox.Show("Bạn Đã Giành Chiến Thắng.");
+                                    lb_Cauhoi.Visible = false;
+                                    gb_Dapan.Visible = false;
+                                    bt_Thoatgame.Visible = true;
+                                    bt_Next.Visible = true;
+
+                                    ptb_anhDauGame.Visible = true;
+                                    gb_luachongoi_cauhoi.Visible = true;
+                                    gb_Danhsach_cauhoi.Visible = false;
+
+                                }
+                                check = true;
+                                break;
+                            }
+                        }
+                        if (check == false)
+                        {
+                            MessageBox.Show("Lụa chọn không chính xác.");
+                        }
+                    }
+                }
+            }
+        }
+        private void bt_Next_Click(object sender, EventArgs e)
+        {
+            Index++;
+            lb_Cauhoi.Text = Danh_Sach_Cau_Hoi[Index]; // đang sử dụng dialogresulf.ok
+            ChiSo_xetDKLocation = 0;
+            arr_rb.Clear();
+            gb_Dapan.Controls.Clear();
+            bt_Next.Enabled = false;
+
+            TaoRadioButton();
+
+            arr_gb_Cauhoi[Index - 1].Enabled = true;
+            arr_gb_Cauhoi[Index - 1].BackColor = Color.Green;
+            arr_gb_Cauhoi[Index].BackColor = Color.LightPink;
+        }
+
+        private void rb_cauhoi_hacknao_CheckedChanged(object sender, EventArgs e)
+        {
+            bt_Next.Enabled = false;
+            Danh_Sach_Cau_Hoi.Clear();
+            Danh_Sach_Dap_An_Theo_Cau.Clear();
+            arr_rb.Clear();
+            gb_Dapan.Controls.Clear();
+            ChiSo_xetDKLocation = 0;
+            Index = 0;
+
+            if (rb_cauhoi_hacknao.Checked == true)
+            {
+                foreach (var item in File.ReadLines(Dia_chi_Cau_Cau_Hoi_HackNao)) // đọc câu hỏi từ file
+                {
+                    if (item.Contains("A.") || item.Contains("B.") || item.Contains("C.") || item.Contains("D."))
+                    {
+                        abc.Add(item); // đưa ựa chọn vào abc
+                    }
+                    else if (item != "")
+                    {
+                        Danh_Sach_Cau_Hoi.Add(item); // đưa câu hỏi vào danh sách
+                        if (abc.Count != 0)
+                        {
+                            // cần tạo thêm 1 mảng gán bằng abc và thêm vào danh sách đáp án theo câu. nếu không kho clear abc thì danh sách đáp án theo câu sẻ bị rỗng
+                            Danh_Sach_Dap_An_Theo_Cau.Add(new List<String>(abc));// không dduocj thêm trực tiếp abc
+                            abc.Clear();
+                        }
+                    }
+                }
+                if (Danh_Sach_Cau_Hoi.Count != Danh_Sach_Dap_An_Theo_Cau.Count)// đưa các lựa chọn câu hỏi cuối cùng vào
+                {
+                    Danh_Sach_Dap_An_Theo_Cau.Add(new List<String>(abc));
+                    abc.Clear();
+                }
+
+                foreach (var item3 in File.ReadAllLines(Dia_Chi_Dap_An_HackNao)) // đưa đáp án vào mảng từ file
+                {
+                    arr_Dapan_Vui.Add(item3);
+                }
+                Hanhdongkhicheck_true();
+            }
+        }
+
+        private void bt_Thoatgame_Click(object sender, EventArgs e)
+        {
+            lb_Cauhoi.Visible = false;
+            gb_Dapan.Visible = false;
+            bt_Thoatgame.Visible = false;
+            bt_Next.Visible = false;
+            ptb_anhDauGame.Visible = true;
+            gb_luachongoi_cauhoi.Visible = true;
+            Danh_Sach_Cau_Hoi.Clear();
+            Danh_Sach_Dap_An_Theo_Cau.Clear();
+            arr_rb.Clear();
+            gb_Dapan.Controls.Clear();
+            ChiSo_xetDKLocation = 0;
+            Index = 0;
+            arr_gb_Cauhoi.Clear();
+            ChiSo_xetDKLocation2 = 0;
+            this.gb_Danhsach_cauhoi.Controls.Clear();
+            gb_Danhsach_cauhoi.Visible = false;
+        }        
+    }
+}
